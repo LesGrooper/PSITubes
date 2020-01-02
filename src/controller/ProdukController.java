69ae0a7e4ae85e9bab445e4fa5bc0fc284c66721
LesGrooper <eqimulia@mail.unpas.ac.id>
2020-01-02 21:13:12 +0700

    masuk3

--- src/controller/ProdukController.java
+++ src/controller/ProdukController.java
@@ -3,23 +3,15 @@ package controller;
 import view.SideBarMenu;
 
 public class ProdukController {
-<<<<<<< HEAD
-SideBarMenu sideBarMenu;
+
+    SideBarMenu sideBarMenu;
+
     public ProdukController() {
         sideBarMenu = new SideBarMenu();
     }
 
     public int tambahProduk() {
         return tambahProduk();
-=======
-    SideBarMenu sideBarMenu;
-    public ProdukController(){
-        sideBarMenu = new SideBarMenu();
-    }
-    
-    public int tambahPaketProduk(){
-        
-        return tambahPaketProduk();
->>>>>>> b4ee7c6076c4c50c5d438be08cd431fda5c6fc3e
+
     }
 }
